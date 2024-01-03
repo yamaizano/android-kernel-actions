d53ad8a0a5435887e4384ab5a26290d5d2e2141a
Yukizaki Miyamura <96718991+yamaizano@users.noreply.github.com>
2024-01-03 18:48:43 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -196,7 +196,9 @@ else
     exit 100
 fi
 
-cd "$workdir"/"$kernel_path"; git submodule update --init --recursive; git submodule update --remote --merge || exit 127
+cd "$workdir"/"$kernel_path" || exit 127
+git submodule update --init --recursive
+git submodule update --recursive --remote
 start_time="$(date +%s)"
 date="$(date +%d%m%Y-%I%M)"
 tag="$(git branch | sed 's/*\ //g')"
